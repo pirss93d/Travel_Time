22c0a20071ce9f1355bf9d9e1cc0f6567cb59e61
Деряев Дмитрий Сергеевич <derjaev_ds@goradm.arz.nnov.ru>
2025-09-25 16:36:57 +0300

    test_textAira

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const btn = document.querySelector(".main_order");
 const btn_on = document.querySelector(".btn_order");
 const btn_push = document.querySelector("#form_btn-push");
 
+//
+const block_help = document.querySelector(".main_block-helper");
+const btn_helper = document.querySelector(".main_help");
+let isOpen_help = false;
+
 // btn.addEventListener("click", function () {
 //   if (!isOpen) {
 //     btn_on.style.display = "block";
@@ -26,4 +31,37 @@ btn.addEventListener("click", function () {
     btn.textContent = "Заказать";
     isOpen = false;
   }
+});
+
+btn_helper.addEventListener("click", function () {
+  if (!isOpen_help) {
+    block_help.style.display = "flex";
+    isOpen_help = true;
+  } else {
+    block_help.style.display = "none";
+    isOpen_help = false;
+  }
+});
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // предотвращаем стандартную отправку
+
+  const formData = new FormData(form);
+
+  fetch("/submit", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => {
+      if (response.ok) {
+        alert("Форма успешно отправлена!");
+      } else {
+        alert("Ошибка при отправке формы.");
+      }
+    })
+    .catch(() => {
+      alert("Ошибка сети или сервера.");
+    });
 });
