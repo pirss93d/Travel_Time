b910ee9a7453981f318657f3afd510a32c591bf2
Dmitriy Deryaev <pirss93d@gmail.com>
2025-09-25 07:14:59 +0300

    next

---
+++ app.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".main_order");
+const btn_on = document.querySelector(".btn_order");
+
+btn.addEventListener("click", function () {
+  alert("Клик выполнен");
+});
